4d806e996a16db119c054b71be3db8151778dc58
lexlam1524 <lamkachun1524@gmail.com>
2024-05-30 23:18:40 +1000

    create sign in api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../model/user.js';
 import bcryptjs from "bcryptjs";
 import { errorHandle } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async(req,res,next)=>{
    const {username,email,password} = req.body;
@@ -14,3 +15,17 @@ export const signup = async(req,res,next)=>{
    }
   
 };
+
+export const signin = async(req, res, next)=>{
+   const{email, password} = req.body;
+   try{
+      const validUser = await User.findOne({email});
+      if (!validUser) return next(errorHandle(401,'User not found'));
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if(!validPassword) return (errorHandle(401), 'wrong credentials');
+      const token = jwt.sign({id:validUser._id}, process.env.JWT_SECRET);
+      res.cookie('access_token', token,{httpOnly:true}).status(200).json(validUser);
+   }catch(error){
+      next(error);
+   }
+}
--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
+mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log('connected to mongoDB');
+        app.listen(3000, () => {
+            console.log("Server listening on port 3000");
+          });   
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
 
 const app = express();
 
@@ -24,14 +36,3 @@ app.use((err,req,res,next)=>{
     });
 });
 
-
-mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
-    .then(()=>{
-        console.log('connected to mongoDB');
-        app.listen(3000, () => {
-            console.log("Server listening on port 3000");
-          });   
-    })
-    .catch((err)=>{
-        console.log(err);
-});
