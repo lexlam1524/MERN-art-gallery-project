e2cb7ad86d82b1d6d7d6fb0678f6a6c4a848d023
lexlam1524 <lamkachun1524@gmail.com>
2024-05-30 18:50:40 +1000

    create auth

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 
 mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
     .then(()=>{
@@ -12,10 +13,12 @@ mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.ne
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 
 app.use('/api/user',userRoutes);
-
+app.use('/api/auth',authRoutes);
 
