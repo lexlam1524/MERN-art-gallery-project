aba0214724b2c11803759f747857f73c584536c7
lexlam1524 <lamkachun1524@gmail.com>
2024-05-30 16:26:38 +1000

    create test api routes

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from './routes/user.route.js';
 
 mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
     .then(()=>{
@@ -7,9 +8,14 @@ mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.ne
     })
     .catch((err)=>{
         console.log(err);
-    });
+});
+
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
+
+app.use('/api/user',userRoutes);
+
+
---
+++ backend/routes/user.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { test } from '../controllers/user.controller';
+
+const router = express.Router();
+
+router.get("/",test)
+
+
+export default router;
