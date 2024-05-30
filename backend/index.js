cf6281c802446ad24c0ee989992031a92d2d4798
lexlam1524 <lamkachun1524@gmail.com>
2024-05-30 12:59:16 +1000

    connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://lamkachun1524:hello@backenddb.kciiu3p.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB")
+    .then(()=>{
+        console.log('connected to mongoDB');
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
 const app = express();
 
 app.listen(3000, () => {
